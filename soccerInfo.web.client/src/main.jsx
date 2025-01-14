932470d2784b9ba7c20c454bf8088d83ed1450d4
Wiktor Zielak <wiktorzielak@gmail.com>
2025-01-14 23:13:51 +0100

    refactoring

--- soccerInfo.web.client/src/main.jsx
+++ soccerInfo.web.client/src/main.jsx
@@ -3,7 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+<<<<<<< HEAD
     <StrictMode>
         <App />
     </StrictMode>,
+=======
+    <App />
+>>>>>>> 1c7f9cf (refactoring)
 )
