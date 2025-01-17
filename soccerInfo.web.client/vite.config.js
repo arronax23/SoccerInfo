b7648a792dd4e16aaebfe7d5bb93d14a67fec107
Wiktor Zielak <wiktorzielak@gmail.com>
2025-01-17 18:13:03 +0100

    change vite config for deployment

--- soccerInfo.web.client/vite.config.js
+++ soccerInfo.web.client/vite.config.js
@@ -44,7 +44,7 @@ export default defineConfig({
     server: {
         proxy: {
             '^/api': {
-                target: 'https://localhost:7109/',
+                target: 'http://localhost:5000/',
                 secure: false
             }
         },
