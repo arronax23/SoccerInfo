61249ec43533907b9feedeb0ba2ccd930e62d1d7
Wiktor Zielak <wiktorzielak@gmail.com>
2025-02-13 14:58:37 +0100

    fix double fetch when bottom of the stats is reached

--- soccerInfo.web.client/src/views/StatsTab/StatsPanel/useTableConfig.jsx
+++ soccerInfo.web.client/src/views/StatsTab/StatsPanel/useTableConfig.jsx
@@ -1,44 +1,54 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { useMaterialReactTable } from "material-react-table";
 
-export default function useTableConfig(columns, stats, loading, incrementPageNumber) {
-    const fetchMoreOnBottomReached = useCallback(
-        (containerRefElement) => {
-          if (containerRefElement) {
-            const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
-            if (
-              scrollHeight - scrollTop - clientHeight < 100 && !loading
-            ) {
-            incrementPageNumber();
-            // setPageNumber(prevPageNumber => prevPageNumber + 1)
-            }
-          }
-        },
-        [loading],
-      );
-    
+export default function useTableConfig(
+  columns,
+  stats,
+  loading,
+  incrementPageNumber
+) {
+  const enableFetchRef = useRef(false);
 
-    return useMaterialReactTable({
-        columns: columns,
-        data: stats,
-        state: { isLoading: loading },
-        enablePagination: false,
-        enableRowVirtualization: true,
-        muiTableBodyCellProps: {
-          align: "center",
-          sx: {   
-            paddingTop: .5,
-            paddingBottom: .5,
-          },
-        }, 
-        muiTableHeadCellProps: {
-          align: "center",
-        },    
-        muiTableContainerProps: {
-          sx: { overflowY: "auto",overflowX: "hidden", maxHeight: "450px"},
-          onScroll: (e) =>
-            fetchMoreOnBottomReached(e.target),           
-        },
-        enableBottomToolbar: false
-      });
+  const fetchMoreOnBottomReached = useCallback(
+    (containerRefElement) => {
+      if (containerRefElement && !loading && enableFetchRef.current) {
+        const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
+        if (scrollHeight - scrollTop - clientHeight < 200 && !loading) {
+          enableFetchRef.current = false;
+          incrementPageNumber();
+        }
+      }
+    },
+    [loading, incrementPageNumber]
+  );
+
+
+  useEffect(() => {
+    if (!loading) {
+      enableFetchRef.current = true; 
+    }
+  }, [loading]); 
+
+  return useMaterialReactTable({
+    columns: columns,
+    data: stats,
+    state: { isLoading: loading },
+    enablePagination: false,
+    enableRowVirtualization: true,
+    muiTableBodyCellProps: {
+      align: "center",
+      sx: {
+        paddingTop: 0.5,
+        paddingBottom: 0.5,
+      },
+    },
+    muiTableHeadCellProps: {
+      align: "center",
+    },
+    muiTableContainerProps: {
+      sx: { overflowY: "auto", overflowX: "hidden", maxHeight: "450px" },
+      onScroll: (e) => fetchMoreOnBottomReached(e.target),
+    },
+    enableBottomToolbar: false,
+  });
 }
