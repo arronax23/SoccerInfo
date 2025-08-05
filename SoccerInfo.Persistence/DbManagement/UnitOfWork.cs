4fe39ac4f1fa930576f55c47fb067fefe95da58e
Wiktor Zielak <wiktorzielak@gmail.com>
2025-08-05 21:15:29 +0200

    refactoring

--- SoccerInfo.Persistence/DbManagement/UnitOfWork.cs
+++ SoccerInfo.Persistence/DbManagement/UnitOfWork.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
-using SoccerInfo.Application.Intrefaces;
+using SoccerInfo.Application.Interfaces;
 using SoccerInfo.Persistence.Data;
 using SoccerInfo.Persistence.EntityFrameworkExtensions;
 
