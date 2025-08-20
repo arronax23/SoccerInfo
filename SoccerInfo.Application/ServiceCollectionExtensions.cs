8b10e44a1ca1075547df0550c6453f10f64705dc
Wiktor Zielak <wiktorzielak@gmail.com>
2025-08-20 19:58:52 +0200

    refactoring

--- SoccerInfo.Application/ServiceCollectionExtensions.cs
+++ SoccerInfo.Application/ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@
 using Polly;
 using SoccerInfo.Application.Commands.ExtractMarketValueProgress;
 using SoccerInfo.Application.Commands.ExtractNationalities;
-using SoccerInfo.BackendScraper;
 
 namespace SoccerInfo.Application;
 public static class ServiceCollectionExtensions
